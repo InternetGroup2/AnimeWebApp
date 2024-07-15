fdc383dc9a6882cb9dcb28d5e4465666bd1f1146
EmmaDz <emmazdaa@gmail.com>
2024-07-14 20:09:17 -0700

    git@github.com:InternetGroup2/AnimeWebApp.git

--- obj/Debug/net8.0/AnimeWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnimeWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimeWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a62431521fa5d91fa6808b422b381af56f049ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c858f3266ea92a46d1e75ca54f2948ea0fb5f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimeWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimeWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
